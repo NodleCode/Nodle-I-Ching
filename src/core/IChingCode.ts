bf2fbee0333beb2f3604ee563709a8a6591d3635
Abdelrahman Ramadan <abdelrahman@elysian.team>
2018-09-02 00:58:45 +0200

    helper interfaces

---
+++ src/core/IChingCode.ts
@@ -0,0 +1,25 @@
+import { Point } from "./Point";
+
+/**
+ * Interface representing the decoded IChing code and information about the image.
+ *
+ * @export
+ * @interface IChingCode
+ */
+export interface IChingCode {
+    /**
+     * IChing code version number
+     */
+    version: number;
+    /**
+     *
+     */
+    data: string;
+    binary: number[];
+    finders: {
+        topRight: Point;
+        topLeft: Point;
+        bottomRight: Point;
+        bottomLeft: Point;
+    };
+}
---
+++ src/core/Point.ts
@@ -0,0 +1,11 @@
+
+/**
+ * Interface to represent 2D point on image grid - could have floating-point coordinates
+ *
+ * @export
+ * @interface Point
+ */
+export interface Point {
+    x: number;
+    y: number;
+}
