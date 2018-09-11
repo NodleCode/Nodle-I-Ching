2c8c4cb3b15a98a183647d2a2c38e7fde29d28c0
osama <osama@elysian.team>
2018-09-11 18:58:08 +0200

    Create Extractor class

---
+++ src/core/decoder/extractor/Extractor.ts
@@ -0,0 +1,304 @@
+import { BitMatrix } from "../../BitMatrix";
+import { EncodedIChing } from "../../EncodedIChing";
+import { Encoder } from "../../encoder";
+import { Point } from "../../Point";
+import { Writer } from "../../writer";
+
+/**
+ * This class extracts raw data from a perspective corrected binary image,
+ * represented as a BitMatrix.
+ *
+ * @export
+ * @class Extractor
+ */
+export class Extractor {
+    // Thresholds used for different checks in the class. Only used internally.
+    private static VERTICAL_BORDER_BLACK_THRESHOLD = 0.4;
+    private static LINE_VALID_BLACK_THRESHOLD = 0.5;
+    private static LINE_ONE_BLACK_THRESHOLD = 0.9;
+    private static GAP_DIM_THRESHOLD = 0.7;
+    private static UNIT_DIM_THRESHOLD = 0.7;
+
+    // Constants representing different line states. Only used internally.
+    private static LINE_STATE_INVALID = -1;
+    private static LINE_STATE_WHITE = 0;
+    private static LINE_STATE_BLACK = 1;
+
+    /**
+     * Main class method. Extracts encoded data from the given perspective corrected binary image.
+     *
+     * @param {BitMatrix} matrix - binary image represented as bits.
+     * @returns {EncodedIChing}
+     */
+    public extract(matrix: BitMatrix): EncodedIChing {
+        const imgWidth = matrix.width;
+        const imgHeight = matrix.height;
+
+        // TODO: implement a better estimation method, as the current one fails if the centre
+        // of the finder patterns is not perfectly aligned with the corners of the image.
+        const scale = this.estimateScale(matrix);
+        const scaledUnitDim = Writer.UNIT_DIM * scale;
+        const scaledSymbolDim = Writer.SYMBOL_DIM * scale;
+        const scaledGapDim = Writer.GAP_DIM * scale;
+        const scaledFinderRadius = Writer.FINDER_OUTER_RADIUS * scale;
+
+        // Calculate width and height of code, in symbols. Should match specs.
+        const cols = Math.round((imgWidth + scaledGapDim - scaledSymbolDim) /
+            (scaledGapDim + scaledSymbolDim));
+        const rows = Math.round((imgHeight + scaledGapDim - scaledSymbolDim) /
+            (scaledGapDim + scaledSymbolDim));
+
+        if (cols !== Encoder.COLS || rows !== Encoder.ROWS) {
+            throw new Error("IChing code does not match specs!");
+        }
+
+        const data = new Uint8ClampedArray(rows * cols);
+
+        // Column-by-column vertical scan.
+        for (let col = 0; col < cols; col++) {
+            // Estimated coordinates for the first symbol in the column.
+            let estimateX1 = Math.round(scaledFinderRadius +
+                col * (scaledSymbolDim + scaledGapDim));
+            let estimateX2 = Math.round(estimateX1 + scaledSymbolDim);
+            let estimateY1 = Math.round(scaledFinderRadius);
+            let estimateY2 = Math.round(estimateY1 + scaledSymbolDim);
+
+            // Y-coordinate of scanned line.
+            let scanY = estimateY1;
+
+            for (let row = 0; row < rows; row++) {
+                // Fix potential horizontal shift resulting from distortion.
+                const horizontalShift = this.fixHorizontalShift(matrix,
+                    estimateX1, estimateY1, estimateX2, estimateY2);
+                const x1 = horizontalShift[0];
+                const x2 = horizontalShift[1];
+
+                // Scanned line states bookkeeping.
+                let endOfSymbol: boolean = false;
+                let oldState = Extractor.LINE_STATE_INVALID;
+                let oldStateCount = 1;
+                let oldStateStartY;
+
+                // Symbol data bookkeeping.
+                let bitsFound = 0;
+                let mask = 0;
+
+                // Symbol coordinates bookkeeping.
+                const symbolX1 = x1;
+                const symbolX2 = x2;
+                let symbolY1;
+                let symbolY2;
+
+                while (scanY < matrix.height && !endOfSymbol) {
+                    const newState = this.getLineState(matrix, x1, scanY, x2, scanY);
+                    if (newState === oldState) { // Same state.
+                        oldStateCount++;
+                    } else { // Different state.
+                        // Check if old state is a valid bit.
+                        if (oldState !== Extractor.LINE_STATE_INVALID &&
+                        oldStateCount / scaledUnitDim > Extractor.UNIT_DIM_THRESHOLD) {
+                            mask |= oldState << bitsFound;
+                            bitsFound++;
+
+                            // If first bit of the symbol, store the y-coordinate
+                            // of the top of the symbol.
+                            if (bitsFound === 1) {
+                                symbolY1 = oldStateStartY;
+                            }
+                        }
+
+                        oldState = newState;
+                        oldStateCount = 1;
+                        oldStateStartY = scanY;
+                    }
+
+                    // Check if end-of-symbol is reached.
+                    if (oldState === Extractor.LINE_STATE_INVALID &&
+                    oldStateCount / scaledGapDim > Extractor.GAP_DIM_THRESHOLD) {
+                        endOfSymbol = true;
+                    }
+
+                    scanY++;
+                }
+
+                data[row * cols + col] = mask;
+
+                // Y-coordinate of the bottom of the symbol.
+                symbolY2 = oldStateStartY;
+
+                // Calculate estimated coordinates for the next symbol based on the current one.
+                estimateX1 = symbolX1;
+                estimateX2 = symbolX2;
+                estimateY1 = Math.round(symbolY2 + scaledGapDim);
+                estimateY2 = Math.round(estimateY1 + symbolY2 - symbolY1);
+            }
+        }
+
+        return { version: data[0], rows, cols, data };
+    }
+
+    /**
+     * Estimates a scale of the input image relative to the default IChing image resolution, based
+     * on the average radius of the finder patterns.
+     *
+     * @param {BitMatrix} matrix - binary image represented as bits.
+     * @returns {number} estimated scale.
+     */
+    private estimateScale(matrix: BitMatrix): number {
+        let radiusSum = 0;
+
+        // Top-left finder, horizontal.
+        radiusSum += this.scanFinderRadius(matrix, { x: 0, y: 0 }, 1, 0);
+        // Top-left finder, vertical.
+        radiusSum += this.scanFinderRadius(matrix, { x: 0, y: 0 }, 0, 1);
+
+        // Bottom-left finder, horizontal.
+        radiusSum += this.scanFinderRadius(matrix, { x: 0, y: matrix.height - 1 }, 1, 0);
+        // Bottom-left finder, vertical.
+        radiusSum += this.scanFinderRadius(matrix, { x: 0, y: matrix.height - 1 }, 0, -1);
+
+        // Top-right finder, horizontal.
+        radiusSum += this.scanFinderRadius(matrix, { x: matrix.width - 1, y: 0 }, -1, 0);
+        // Top-right finder, vertical.
+        radiusSum += this.scanFinderRadius(matrix, { x: matrix.width - 1, y: 0 }, 0, 1);
+
+        const averageRadius = radiusSum / 6;
+        const estimatedScale = averageRadius / Writer.FINDER_OUTER_RADIUS;
+
+        return estimatedScale;
+    }
+
+    /**
+     * Returns the radius of the finder pattern, given its centre and a scanning direction,
+     * horizontal or vertical.
+     *
+     * @param {BitMatrix} matrix - binary image represented as bits.
+     * @param {Point} centre
+     * @param {number} dx
+     * @param {number} dy
+     * @returns {number} radius
+     */
+    private scanFinderRadius(matrix: BitMatrix, centre: Point, dx: number, dy: number): number {
+        if (!(Math.abs(dx) === 1 && dy === 0) && !(dx === 0 && Math.abs(dy) === 1)) {
+            throw new Error("Scan direction is neither horizontal nor vertical!");
+        }
+
+        let state = 0;
+        let radius = 0;
+        let x = centre.x;
+        let y = centre.y;
+        while (state < 3) {
+            radius++;
+            x += dx;
+            y += dy;
+            if ((state & 1) === matrix.get(x, y)) {
+                state++;
+            }
+        }
+
+        return radius;
+    }
+
+    /**
+     * Takes coordinates representing a sub-rectangle of the image containing an IChing symbol,
+     * and fixes the horizontal shift, if any, in the left and right borders of the sub-rectangle.
+     *
+     * @param {BitMatrix} matrix - binary image represented as bits.
+     * @param {number} x1
+     * @param {number} y1
+     * @param {number} x2
+     * @param {number} y2
+     * @returns {number[]} [fixed x1, fixed x2]
+     */
+    private fixHorizontalShift(
+        matrix: BitMatrix, x1: number, y1: number, x2: number, y2: number,
+    ): number[] {
+        const threshold = Extractor.VERTICAL_BORDER_BLACK_THRESHOLD;
+        const boxHeight = y2 - y1 + 1;
+        const width = matrix.width;
+
+        // Fix left border.
+        while (x1 >= 0 && x1 < width &&
+            this.countBlackInLine(matrix, x1, y1, x1, y2) / boxHeight > threshold) {
+                x1--;
+        }
+        while (x1 >= 0 && x1 < width &&
+            this.countBlackInLine(matrix, x1, y1, x1, y2) / boxHeight < threshold) {
+                x1++;
+        }
+
+        // Fix right border.
+        while (x2 >= 0 && x2 < width &&
+            this.countBlackInLine(matrix, x2, y1, x2, y2) / boxHeight > threshold) {
+                x2++;
+        }
+        while (x2 >= 0 && x2 < width &&
+            this.countBlackInLine(matrix, x2, y1, x2, y2) / boxHeight < threshold) {
+                x2--;
+        }
+
+        return [x1, x2];
+    }
+
+    /**
+     * Counts the number of black and white pixels in a vertical or horizontal line.
+     *
+     * @param {BitMatrix} matrix - binary image represented as bits.
+     * @param {number} x1
+     * @param {number} y1
+     * @param {number} x2
+     * @param {number} y2
+     * @returns {number} count of black pixels in given line.
+     */
+    private countBlackInLine(
+        matrix: BitMatrix, x1: number, y1: number, x2: number, y2: number,
+    ): number {
+        if (x1 !== x2 && y1 !== y2) {
+            throw new Error("Line must be horizontal or vertical!");
+        }
+
+        let count: number = 0;
+        for (let x = x1; x <= x2; x++) {
+            for (let y = y1; y <= y2; y++) {
+                count += matrix.get(x, y);
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Checks if the given horizontal line belongs to one of the bits of an IChing symbol,
+     * and if so, whether it is a zero or a one.
+     *
+     * @param {BitMatrix} matrix - binary image represented as bits.
+     * @param {number} x1
+     * @param {number} y1
+     * @param {number} x2
+     * @param {number} y2
+     * @returns {number} one of the three LINE_STATE constants.
+     */
+    private getLineState(
+        matrix: BitMatrix, x1: number, y1: number, x2: number, y2: number,
+    ): number {
+        if (y1 !== y2) {
+            throw new Error("Line must be horizontal!");
+        }
+
+        const lineWidth = x2 - x1 + 1;
+        const black = this.countBlackInLine(matrix, x1, y1, x2, y2);
+
+        // Check if the line belongs to one of the black lines of an IChing symbol.
+        if (black / lineWidth < Extractor.LINE_VALID_BLACK_THRESHOLD) {
+            return Extractor.LINE_STATE_INVALID;
+        }
+
+        // Check if line represents a zero-bit.
+        if (black / lineWidth < Extractor.LINE_ONE_BLACK_THRESHOLD) {
+            return Extractor.LINE_STATE_WHITE;
+        }
+
+        // Line represents a one-bit.
+        return Extractor.LINE_STATE_BLACK;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { AdaptiveBinarizer } from "./core/decoder/binarizer";
 export { Decoder } from "./core/decoder";
 export { EncodedIChing } from "./core/EncodedIChing";
 export { Encoder } from "./core/encoder";
+export { Extractor } from "./core/decoder/extractor";
 export { ImageData } from "./core/ImageData";
 export { Locator } from "./core/decoder/locator/Locator";
 export { ReedSolomonDecoder } from "./core/common/reedsolomon";
