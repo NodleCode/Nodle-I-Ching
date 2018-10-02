71702884c50ebfb08ebb2468deea35e62c2ee270
Osama Hatem Sharafeldin <osama@oroboro.studio>
2018-10-02 22:11:34 +0200

    Reset ./src/tests/e2e_tests/e2e.test.ts

--- src/tests/e2e_tests/e2e.test.ts
+++ src/tests/e2e_tests/e2e.test.ts
@@ -1,11 +1,11 @@
 import { decode } from "../..";
 import { loadJpeg } from "../fileHelpers";
 
-const testsCount = 10;
+const testsCount = 82;
 const testPath = (testNumber: number) => `./src/tests/test_data/camera_images/${testNumber}.jpg`;
 
 describe("End to End testing scenarios", async () => {
-    for (let test = 1 + testsCount * 4; test <= testsCount * 8; ++test) {
+    for (let test = 1; test <= testsCount; ++test) {
         it("Decodes sample " + test + " correctly!", async () => {
             const img = await loadJpeg(testPath(test));
             decode(img.data, img.width, img.height);
